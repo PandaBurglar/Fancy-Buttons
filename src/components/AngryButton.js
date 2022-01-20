d1d987c68da97192980dbbc51f162bb598ab98e3
root <root@PANDABURGLAR99.localdomain>
2022-01-20 15:20:42 -0500

    components updated to use state hook

---
+++ src/components/AngryButton.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+/* For this button, we want the value of anger to go from 0 to 1 by increments of 0.1, and once we go over 1, reset it to zero. We need to use the value of anger in two places:
+
+  1. for the background color of the button, and
+  2. as a condition that will determine which message is displayed. 
+*/
+function AngryButton(props){
+  const [anger, setAnger] = useState();
+  return (
+    <button style={{backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
+        {anger < 1 && <span>Don't click me too much! </span>}
+        { anger > 1 && <span>Rawr!</span>}
+      </button>
+  );
+}
+
+export default AngryButton;
---
+++ src/components/CounterButton.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+
+function CounterButton(props){
+    const [clickAmount, setClickAmount] = useState();
+    return (
+      <button className="CounterButton">
+        You clicked me X amount of times
+      </button>
+    );
+  }
+
+  export default CounterButton;
